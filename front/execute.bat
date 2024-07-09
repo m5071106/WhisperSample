2c779ea3de53f75a924caf5be955b64c333edc80
m5071106 <m5071106_clavier@yahoo.co.jp>
2024-07-09 16:40:34 +0900

    rename source directory

--- front/execute.bat
+++
@@ -1,6 +0,0 @@
-cd /d %~dp0
-python3 conv_mp4_to_mp3\conv_mp4_to_mp3.py
-python3 audio_splitter\audio_splitter.py
-python3 sound_trans\sound_trans.py
-python3 text_coordinater\text_coordinater.py
-pause
--- front/front_web/js/front_web.js
+++
@@ -1,7 +0,0 @@
-function init() {
-    document.getElementById("processing").style.display = "none";
-}
-
-function upload() {
-document.getElementById("processing").style.display = "block";
-}
